1d9de21e89408bfd6f378aa1b4639e8f19377126
RomeroCJR <115511502+RomeroCJR@users.noreply.github.com>
2022-10-10 20:02:37 -0400

    Primera subida

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <title>Document</title>
+</head>
+<body>
+    <div class="row">
+        <div class="col s6 red" style="height: 100%">
+            <h5>HOLA</h5>
+        </div>
+        <div class="col s6 blue" style="height: 100%">
+            <h5>MUNDO</h5>
+        </div>
+    </div>
+</body>
+</html>
