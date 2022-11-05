94cc914dec5808f9367666d90b613056e5904e08
cristianmc9000 <cristian.preof1@gmail.com>
2022-11-05 16:21:21 -0400

    página

--- inicio.php
+++ inicio.php
@@ -6,14 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="images/logo.png" />
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" >
     <!-- Compiled and minified CSS -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">    
+    <script src="js/jquery-3.0.0.min.js"></script>
     <!-- Compiled and minified JavaScript -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
    
     <title>Repostería KRUS-MARY</title>
 </head>
 <style>
+    html{
+        scroll-behavior: smooth;
+    }
     @media only screen and (min-width : 992px) {
         .slider{
             height: 100vh !important;
@@ -23,6 +28,12 @@
         }
         .indicators{
             z-index:999;
+            padding-bottom:10px;
+        }
+        .slides li img {
+            /* max-width: 100% !important; */
+            /* height: 100% !important; */
+            background-attachment: fixed !important;
         }
 	}
     #logo{
@@ -50,11 +61,64 @@
         align-items: center;
         
    }
+
+   .grid-item:hover{
+    filter: opacity(0.9);
+    transform: scale(1.04);
+   }
+   .grid-container{
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+    grid-auto-rows: minmax(200px, auto);
+    gap:20px;
+    padding: 20px;
+    grid-auto-flow: dense;
+   }
+
+   @media (min-width: 600px){
+    .wide{
+        grid-column: span 2;
+    }
+    .tall{
+        grid-row: span 2;
+    }
+   }
+
+   @media (max-width: 992px){
+    .dropdown-trigger i{
+        padding-right: 15px;
+    }
+   }
+   @media (max-width: 600px){
+    .dropdown-trigger i{
+        padding: 15px;
+    }
+   }
+   
+
+    
+
+
 </style>
 <body>
-    <div class="" style="position:fixed; z-index:9999; width:100%;">
+    <ul id="dropdown1" class="dropdown-content" >
+        <li><a href="#section-1" class="waves-effect waves-light black-text">Inicio</a></li>
+        <li><a href="#section-2" class="waves-effect waves-light black-text">Galería</a></li>
+        <li><a href="#section-3" class="waves-effect waves-light black-text">Nuestros productos</a></li>
+        <li><a href="#section-4" class="waves-effect waves-light black-text">Contáctanos</a></li>
+    </ul>
+    <div class="" style="position:fixed; z-index:9999; width:100%; top:0px;">
         <nav style="background: rgba(255,255,255, 0.6); ">
             <div class="nav-wrapper">
+                <ul class="left hide-on-large-only">
+                    <li ><a class="black-text dropdown-trigger" href="#!" data-target="dropdown1">Menú<i class="material-icons-outlined right">arrow_drop_down</i></a></li>
+                </ul>
+                <ul class="left hide-on-med-and-down">
+                    <li><a href="#section-1" class="waves-effect waves-light black-text">Inicio</a></li>
+                    <li><a href="#section-2" class="waves-effect waves-light black-text">Galería</a></li>
+                    <li><a href="#section-3" class="waves-effect waves-light black-text">Nuestros productos</a></li>
+                    <li><a href="#section-4" class="waves-effect waves-light black-text">Contáctanos</a></li>
+                </ul>
                 <a href="#" class="brand-logo center"><img id="logo" src="img/logo.png" alt=""></a>
                 <ul class="right">
                     <li><a href="registro.php" class="roboto" style="color:black;">Ingresa o Regístrate</a></li>
@@ -63,46 +127,102 @@
         </nav>  
     </div>  
 
-    <div class="slider" >
-        <ul class="slides" >
-            <li >
-                <img src="images/fondo1.png"> <!-- random image -->
-                <div class="caption left-align black-text ">
-                    <h3>Repostería KRUS-MARY!</h3>
-                    <h5 class=""><b>Las mejores tortas para cumpleaños...</b></h5>
+    <section id="section-1">
+        <div class="slider " >
+            <ul class="slides" >
+                <li >
+                    <img src="images/fondo1.png" style="width:100% !important;"> <!-- random image -->
+                    <div class="caption left-align black-text ">
+                        <h3>Repostería KRUS-MARY!</h3>
+                        <h5 class=""><b>Las mejores tortas para cumpleaños...</b></h5>
+                    </div>
+                </li>
+                <li>
+                    <img src="images/fondo1.jpg"> <!-- random image -->
+                    <div class="caption center-align ">
+                    <!-- <h3>Repostería KRUS-MARY!</h3> -->
+                    <h5 class=""><b>Tortas y Pasteles, preparados a gusto y necesidad del cliente.</b></h5>
+                    </div>
+                </li>
+                <li>
+                    <img src="images/fondo2.png"> <!-- random image -->
+                    <div class="caption right-align black-text">
+                    <!-- <h3>Repostería KRUS-MARY!</h3> -->
+                    <h5 class="light black-text text-lighten-3">Gran variedad de postres, para tú elección.</h5>
+                    </div>
+                </li>
+                <li>
+                    <img src="images/fondo3.jpg"> <!-- random image -->
+                    <div class="caption center-align black-text">
+                    <!-- <h3>Repostería KRUS-MARY!</h3> -->
+                    <br><br><br><br><br>
+                    <h5 class=""><b>Llámanos o haz una reserva desde la página</b> &#128512;.</h5>
+                    </div>
+                </li>
+                
+            </ul>
+            
+        </div>
+    </section>
+    <section id="section-2">
+        <div class="container" >
+            <h3 class="rubik">Galería</h3>
+            <div class="grid-container">
+                <!-- <div class="z-depth-3 grid-item" ><img src="images/481ad81b-feda-4b43-b34c-df87d6455427.jpg" class="materialboxed" width="100%" alt=""></div>
+                <div class="z-depth-3 grid-item wide tall" ><img src="images/708c9fe9-a129-4ac9-b5fa-ecebdc268607.jpg" class="materialboxed" width="100%" alt=""></div>
+                <div class="z-depth-3 grid-item wide tall" ><img src="images/7387c9dc-ddd5-43bd-831f-04a870552328.jpg" class="materialboxed" width="100%" alt=""></div>
+                <div class="z-depth-3 grid-item" ><img src="images/18620064_501200630271525_7595693900660456076_n.png" class="materialboxed" width="100%" alt=""></div>
+                <div class="z-depth-3 grid-item" ><img src="images/d7d89023-1ce1-4cf1-b3fb-1d2d3dcf7993.jpg" class="materialboxed" width="100%" alt=""></div>
+                <div class="z-depth-3 grid-item" ><img src="images/f35c10d7-72b1-4168-934b-f618b97a9f9a.jpg" class="materialboxed" width="100%" alt=""></div>
+                <div class="z-depth-3 grid-item" ><img src="images/PXL_20220722_232513850.MP.jpg" class="materialboxed" width="100%" alt=""></div>
+                <div class="z-depth-3 grid-item" ><img src="images/18581799_501200553604866_5054816197699773601_n.jpg" class="materialboxed" width="100%" alt=""></div>
+                -->
+                
+                
+                
+                <div class="z-depth-3 grid-item " style="background-size: 100%; background-repeat: no-repeat;background-image: url('images/481ad81b-feda-4b43-b34c-df87d6455427.jpg')"></div>
+                <div class="z-depth-3 grid-item wide tall" style="background-size: 100%; background-repeat: no-repeat;background-image: url('images/708c9fe9-a129-4ac9-b5fa-ecebdc268607.jpg')"></div>
+                <div class="z-depth-3 grid-item wide tall " style="background-size: 100%; background-repeat: no-repeat;background-image: url('images/7387c9dc-ddd5-43bd-831f-04a870552328.jpg')"></div>
+                <div class="z-depth-3 grid-item" style="background-size: 100%; background-repeat: no-repeat;background-image: url('images/18620064_501200630271525_7595693900660456076_n.png')"></div>
+                <div class="z-depth-3 grid-item" style="background-size: 100%; background-repeat: no-repeat;background-image: url('images/d7d89023-1ce1-4cf1-b3fb-1d2d3dcf7993.jpg')"></div>
+                <div class="z-depth-3 grid-item" style="background-size: 100%; background-repeat: no-repeat;background-image: url('images/f35c10d7-72b1-4168-934b-f618b97a9f9a.jpg')"></div>
+                <div class="z-depth-3 grid-item " style="background-size: 100%; background-repeat: no-repeat;background-image: url('images/PXL_20220722_232513850.MP.jpg')"></div>
+                <div class="z-depth-3 grid-item " style="background-size: 100%; background-repeat: no-repeat;background-image: url('images/18581799_501200553604866_5054816197699773601_n.jpg')"></div>
+                
+            </div>
+        </div>
+    </section>
+    <section id="section-3">
+        <div class="" style="background-color: #fce4ec">
+            <div class="container row" id="cards_row" >
+            <h3 class="rubik">Nuestros productos</h3>
+                <div class="col s12 m4">
+                    <div class="card-panel light-blue darken-2">
+                        <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                        </span>
+                    </div>
                 </div>
-            </li>
-            <li>
-                <img src="images/fondo1.jpg"> <!-- random image -->
-                <div class="caption center-align ">
-                <!-- <h3>Repostería KRUS-MARY!</h3> -->
-                <h5 class=""><b>Tortas y Pasteles, preparados a gusto y necesidad del cliente.</b></h5>
+                <div class="col s12 m4">
+                    <div class="card-panel light-blue darken-2">
+                        <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                        </span>
+                    </div>
                 </div>
-            </li>
-            <li>
-                <img src="images/fondo2.png"> <!-- random image -->
-                <div class="caption right-align black-text">
-                <!-- <h3>Repostería KRUS-MARY!</h3> -->
-                <h5 class="light black-text text-lighten-3">Gran variedad de postres, para tú elección.</h5>
+                <div class="col s12 m4">
+                    <div class="card-panel light-blue darken-2">
+                        <span class="white-text">I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively. I am similar to what is called a panel in other frameworks.
+                        </span>
+                    </div>
                 </div>
-            </li>
-            <li>
-                <img src="images/fondo3.jpg"> <!-- random image -->
-                <div class="caption center-align black-text">
-                <!-- <h3>Repostería KRUS-MARY!</h3> -->
-                <br><br><br><br><br>
-                <h5 class=""><b>Llámanos o haz una reserva desde la página</b> &#128512;.</h5>
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div class="container">
-        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non explicabo et, modi rerum, magni, nostrum excepturi illum rem pariatur debitis tenetur consequatur facere ratione exercitationem natus. Minima blanditiis quos unde.</span>
-    </div>
-    
+            </div>
+        </div>
+    </section>
 </body>
 
-<footer class="page-footer">
+<footer class="page-footer pink lighten-1" id="section-4">
     <div class="container">
         <div class="row">
 
@@ -132,6 +252,9 @@
     // let options = {'height': '300'}
     var elems = document.querySelectorAll('.slider');
     var instances = M.Slider.init(elems);
+    var elem = document.querySelectorAll('.materialboxed');
+    var instance = M.Materialbox.init(elem);
+    $(".dropdown-trigger").dropdown();
   });
 
 </script>
