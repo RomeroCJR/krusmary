be2f520de307bd40e8c0d677530a6c87642ac1ba
RomeroCJR <115511502+RomeroCJR@users.noreply.github.com>
2022-12-12 18:18:08 -0400

    base de datos

--- recursos/conexion.php
+++ recursos/conexion.php
@@ -1,7 +1,7 @@
 <?php 
 
-$conexion = mysqli_connect('localhost','root','','krusmary');
-// $conexion = new mysqli('localhost','u604223767_krusmary','Cabj1026','u604223767_krusmary'); //agregado desde sublime
+// $conexion = mysqli_connect('localhost','root','','krusmary');
+$conexion = new mysqli('localhost','u604223767_krusmary','Cabj1026','u604223767_krusmary'); //agregado desde sublime
 // $conexion = mysqli_connect('localhost','u604223767_cristian','Desconocido1','u604223767_dppl');
 
 $conexion->query("SET NAMES 'utf8'");
